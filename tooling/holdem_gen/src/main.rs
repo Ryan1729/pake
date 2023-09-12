8e6dde9c0e67913d4f2b887c45da1af2c2b3d6c8
Ryan1729 <Ryan1729@gmail.com>
2023-09-12 01:06:00 -0600

    add EvalCount struct
    The basic idea of the EvalCount struct is to do some amount of trials evaluating hands, probably a random subset of all possible pairings of flops and hands. Then, those counts will be turned into probabilities.

--- tooling/holdem_gen/src/main.rs
+++ tooling/holdem_gen/src/main.rs
@@ -2,13 +2,14 @@ use models::{Card, holdem::{Hand}};
 
 use std::fs::OpenOptions;
 
-// TODO? Import this look_up stuff in some way that doesn't produce a dependency 
+// TODO? Import this look_up stuff in some way that doesn't produce a dependency
 // cycle?
 mod look_up {
     pub mod probability {
         pub type Probability = u8;
         pub const FIFTY_PERCENT: Probability = 0b1000_0000;
         pub const SEVENTY_FIVE_PERCENT: Probability = 0b1100_0000;
+        pub const ONE: Probability = 0b1111_1111;
     }
 
     pub mod holdem {
@@ -17,29 +18,35 @@ mod look_up {
         pub const ALL_SORTED_HANDS_LEN: usize = 1326;
         pub const ALL_SORTED_HANDS: [Hand; ALL_SORTED_HANDS_LEN] = {
             let mut all_hands = [[0; 2]; ALL_SORTED_HANDS_LEN];
-        
+
             let mut index = 0;
-        
+
             let mut i1 = 0;
             while i1 < ALL_CARDS.len() {
                 let mut i2 = i1 + 1;
                 while i2 < ALL_CARDS.len() {
                     let c1 = ALL_CARDS[i1];
                     let c2 = ALL_CARDS[i2];
-        
+
                     all_hands[index] = [c1, c2];
                     index += 1;
                     i2 += 1;
                 }
                 i1 += 1;
             }
-        
+
             all_hands
         };
     }
 }
 
-use look_up::{holdem::ALL_SORTED_HANDS, probability::{Probability, FIFTY_PERCENT}};
+use look_up::{
+    holdem::{
+        ALL_SORTED_HANDS_LEN,
+        ALL_SORTED_HANDS,
+    },
+    probability::{Probability, FIFTY_PERCENT},
+};
 
 type Flop = [Card; 3];
 
@@ -75,6 +82,41 @@ fn probability_for_hand(hand: Hand) -> Probability {
     FIFTY_PERCENT
 }
 
+type Count = u64;
+
+#[derive(Clone, Copy)]
+struct EvalCount {
+    win_count: Count,
+    total: Count,
+}
+
+impl EvalCount {
+    fn probability(self) -> Probability {
+        //assert!(self.total > 0);
+        FIFTY_PERCENT
+    }
+}
+
+#[test]
+fn probability_works_in_these_cases() {
+    use look_up::{probability::{FIFTY_PERCENT, SEVENTY_FIVE_PERCENT, ONE}};
+
+    macro_rules! a {
+        ($numerator: expr , $denomenator: expr => $expected: expr) => ({
+            let eval_count = EvalCount {
+                win_count: $numerator,
+                total: $denomenator,
+            };
+            assert_eq!(eval_count.probability(), $expected);
+        })
+    }
+
+    a!(1, 1 => ONE);
+    a!(0, 1 => 0);
+    a!(1, 2 => FIFTY_PERCENT);
+    a!(3, 4 => SEVENTY_FIVE_PERCENT);
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     use std::io::Write;
 
@@ -86,10 +128,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .create(false)
         .open(WIN_PROBABILTY_OUTPUT_PATH)?;
 
+    let mut eval_counts: [EvalCount; ALL_SORTED_HANDS_LEN] = [
+        EvalCount {
+            win_count: 0,
+            total: 0,
+        };
+        ALL_SORTED_HANDS_LEN
+    ];
+
     write!(file, "[")?;
 
-    for hand in ALL_SORTED_HANDS {
-        write!(file, "{},", probability_for_hand(hand))?;
+    for (i, count) in eval_counts.iter().enumerate() {
+        write!(file, "{},", count.probability())?;
     }
 
     write!(file, "]")?;
