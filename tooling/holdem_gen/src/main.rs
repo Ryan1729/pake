b44c8383827c52d5b84b3bcbb54aa88804007584
Ryan1729 <Ryan1729@gmail.com>
2023-09-16 01:06:00 -0600

    find out that we were evaluating impossible hand/flop combinations
    The changes here help, but doesn't fix the whole problem

--- tooling/holdem_gen/src/main.rs
+++ tooling/holdem_gen/src/main.rs
@@ -66,7 +66,7 @@ fn sorted_flop(index: usize) -> Flop {
         while i1 < ALL_CARDS.len() {
             let mut i2 = i1 + 1;
             while i2 < ALL_CARDS.len() {
-                let mut i3 = i1 + 1;
+                let mut i3 = i2 + 1;
                 while i3 < ALL_CARDS.len() {
                     let c1 = ALL_CARDS[i1];
                     let c2 = ALL_CARDS[i2];
@@ -89,7 +89,7 @@ fn sorted_flop(index: usize) -> Flop {
 
 type Count = u32;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct EvalCount {
     win_count: Count,
     total: Count,
@@ -139,6 +139,8 @@ fn count_evaluation(
     hand_i_2: HandIndex,
     flop_i: FlopIndex,
 ) {
+    assert_ne!(hand_i_1, hand_i_2);
+
     let hand_i_1 = hand_i_1 as usize;
     let hand_i_2 = hand_i_2 as usize;
     let flop_i = flop_i as usize;
@@ -146,6 +148,27 @@ fn count_evaluation(
     let hand_1 = ALL_SORTED_HANDS[hand_i_1];
     let hand_2 = ALL_SORTED_HANDS[hand_i_2];
     let flop = sorted_flop(flop_i);
+
+    {
+        let mut seen = std::collections::HashSet::new();
+
+        let cards = [
+            hand_1[0],
+            hand_1[1],
+            hand_2[0],
+            hand_2[1],
+            flop[0],
+            flop[1],
+            flop[2],
+        ];
+
+        let all_unique = cards
+            .into_iter()
+            .all(move |x| seen.insert(x));
+
+        if !all_unique { return }
+        //assert!(all_unique, "{cards:?} had duplicates");
+    }
 
     let eval_1 = evaluate::holdem_hand(
         CommunityCards::Flop(flop),
@@ -155,21 +178,63 @@ fn count_evaluation(
         CommunityCards::Flop(flop),
         hand_2,
     );
-
+//dbg!(hand_1, hand_2, eval_1, eval_2);
     use core::cmp::Ordering::*;
     match eval_1.cmp(&eval_2) {
         Greater => {
-            counts[hand_i_1].win_count += 1;
+            //dbg!(Greater, hand_i_1);
+            let count = &mut (counts[hand_i_1].win_count);
+            *count = count.saturating_add(1);
         },
         Equal => {},
         Less => {
-            counts[hand_i_2].win_count += 1;
+            //dbg!(Less, hand_i_2);
+            let count = &mut (counts[hand_i_2].win_count);
+            *count = count.saturating_add(1);
         },
     }
 
     counts[hand_i_1].total += 1;
 
     counts[hand_i_2].total += 1;
+}
+
+#[test]
+fn count_evaluation_works_on_these_few_examples() {
+    let mut eval_counts: [EvalCount; ALL_SORTED_HANDS_LEN as usize] = [
+        EvalCount {
+            win_count: 0,
+            total: 0,
+        };
+        ALL_SORTED_HANDS_LEN as usize
+    ];
+
+    const FLOPS_TO_CHECK: FlopCount = 4;
+
+    for hand_i_1 in 0..ALL_SORTED_HANDS_LEN {
+        for hand_i_2 in (hand_i_1 + 1)..ALL_SORTED_HANDS_LEN {
+            for flop_i in 0..4 {
+                count_evaluation(
+                    &mut eval_counts,
+                    hand_i_1,
+                    hand_i_2,
+                    flop_i,
+                );
+            }
+        }
+    }
+
+    let eval_counts = &eval_counts[0..8];
+
+    assert!(
+        !eval_counts.iter().any(|count| count.total <= FLOPS_TO_CHECK),
+        "WARNING: a total was <= {FLOPS_TO_CHECK}\n {eval_counts:?}"
+    );
+
+    assert!(
+        eval_counts.iter().any(|count| count.probability() != 0),
+        "WARNING: all propabilities were == 0\n {eval_counts:?}"
+    );
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -193,6 +258,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             core::mem::transmute::<[f64; 2], [u8; 16]>([time, 1.0 / time])
         }
     };
+    let seed = [173, 113, 107, 104, 41, 63, 217, 65, 113, 10, 152, 191, 164, 71, 4, 62];
 
     println!("{seed:?}");
 
@@ -212,21 +278,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     for output_index in subset.iter_mut() {
         // Pick a (more) normally distributed set by taking the average of N samples.
-        const SAMPLE_PO2: u8 = 16;
+        const SAMPLE_DIGITS: u8 = 4;
+        const SAMPLE_PO2: u8 = 1 << 4;
+        let mut max = 0;
         for _ in 0..SAMPLE_PO2 {
-            *output_index = output_index.saturating_add(xs::range(&mut rng, 0..ALL_SORTED_FLOPS_LEN));
+            let addend = xs::range(&mut rng, 0..ALL_SORTED_FLOPS_LEN);
+            max = std::cmp::max(max, addend);
+            *output_index = output_index.saturating_add(addend);
         }
-        *output_index >>= u32::from(SAMPLE_PO2);
+        *output_index >>= u32::from(SAMPLE_DIGITS);
 
         assert!(*output_index < ALL_SORTED_FLOPS_LEN);
     }
 
     // TODO? Measure whether sorting like this meaningfully improves cache locality?
     subset.sort();
+    {
+        let min = subset[0];
+        let max = subset[subset.len() - 1];
+        assert!(min < max);
+        let middle = ALL_SORTED_FLOPS_LEN / 2;
+        assert!(min < middle, "{min} >= {middle}");
+        assert!(max > middle, "{max} <= {middle}");
+    }
 
     for hand_i_1 in 0..ALL_SORTED_HANDS_LEN {
         println!("{hand_i_1}/{ALL_SORTED_HANDS_LEN}");
-        for hand_i_2 in 0..ALL_SORTED_HANDS_LEN {
+        for hand_i_2 in (hand_i_1 + 1)..ALL_SORTED_HANDS_LEN {
             //println!("    {hand_i_2}/{ALL_SORTED_HANDS_LEN}");
             for flop_i in subset {
                 count_evaluation(
@@ -240,20 +318,40 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     println!("{ALL_SORTED_HANDS_LEN}/{ALL_SORTED_HANDS_LEN}");
 
-    writeln!(file, "// Seed used was: {seed:?}")?;
+    writeln!(file, "// Seed used was: {seed:?}. Used {SUBSET_SIZE}/{ALL_SORTED_FLOPS_LEN} flops")?;
     write!(file, "[")?;
 
     for (i, count) in eval_counts.iter().enumerate() {
         write!(file, "{},", count.probability())?;
     }
 
-    write!(file, "]")?;
+    writeln!(file, "]")?;
 
     // Actually flush to disk before printing the success message.
     file.flush()?;
     drop(file);
 
     println!("wrote win_probabilty to {WIN_PROBABILTY_OUTPUT_PATH}");
+
+    // We'd rather have a bad version on disk that we can examine, than abort
+    // early, given we're looking at things we can't know until the expensive
+    // calculation is done.
+    {   
+        let mut show_eval_counts = false;
+        if eval_counts.iter().any(|count| count.total <= 10) {
+            println!("WARNING: a total was <= 10");
+            show_eval_counts = true;
+        }
+
+        if eval_counts.iter().any(|count| count.probability() == 0) {
+            println!("WARNING: a propability was == 0");
+            show_eval_counts = true;
+        }
+
+        if show_eval_counts {
+            println!("{eval_counts:?}");
+        }
+    }
 
     Ok(())
 }
