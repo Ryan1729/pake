0d687de8cad3d76babfe2e70d904dae1974693d6
Ryan1729 <Ryan1729@gmail.com>
2023-11-09 00:06:00 -0700

    add some selectors to dealer's choice

--- libs/game/src/dealers_choice.rs
+++ libs/game/src/dealers_choice.rs
@@ -12,6 +12,14 @@ use crate::ui::{self, draw_money_in_rect, stack_money_text, ButtonSpec, Id::*, d
 // TODO restrict selection to minimum of selected games' max player count
 type PlayerCount = u8;
 
+// TODO unify the MIN_MONEY_UNIT across different games
+// TODO? Switch to a representation that has MIN_MONEY_UNIT as 1, but scales up for
+//       display only?
+const MIN_MONEY_UNIT: NonZeroMoney = NonZeroMoney::MIN.saturating_add(5 - 1);
+const INITIAL_ANTE_AMOUNT: NonZeroMoney = MIN_MONEY_UNIT.saturating_mul(
+    MIN_MONEY_UNIT
+);
+
 #[derive(Clone)]
 pub enum TableState {
     Undealt { player_count: PlayerCount, starting_money: Money },
@@ -85,8 +93,157 @@ use TableState::*;
                 cmd = ModeCmd::BackToTitleScreen;
             }
 
-            if let Zero = group.ctx.hot {
-                group.ctx.set_next_hot(BackToTitleScreen);
+            // TODO allow selecting which sub-games are allowed to be selected by the dealer
+
+            let player_count_rect = unscaled::Rect {
+                x: unscaled::X(100),
+                y: unscaled::Y(100),
+                w: unscaled::W(50),
+                h: unscaled::H(100),
+            };
+
+    
+            let mut player_count_text = [0 as u8; 20];
+            {
+                use std::io::Write;
+                let _cant_actually_fail = write!(
+                    &mut player_count_text[..],
+                    "{player_count}",
+                );
+            }
+
+            {
+                let xy = gfx::center_line_in_rect(
+                    pre_nul_len(&player_count_text),
+                    player_count_rect,
+                );
+                group.commands.print_chars(
+                    &player_count_text,
+                    xy.x,
+                    xy.y,
+                    TEXT
+                );
+            }
+            {
+                let players_label = b"players";
+
+                let xy = gfx::center_line_in_rect(
+                    players_label.len() as _,
+                    player_count_rect,
+                );
+
+                group.commands.print_chars(
+                    players_label,
+                    xy.x,
+                    xy.y + gfx::CHAR_H,
+                    TEXT
+                );
+            }
+
+            ui::draw_quick_select(
+                group,
+                player_count_rect,
+                PlayerCountSelect,
+            );
+
+            let starting_money_rect = unscaled::Rect {
+                x: unscaled::X(150),
+                y: unscaled::Y(100),
+                w: unscaled::W(50),
+                h: unscaled::H(100),
+            };
+
+            draw_money_in_rect!(group, starting_money, starting_money_rect);
+
+            ui::draw_quick_select(
+                group,
+                starting_money_rect,
+                StartingMoneySelect,
+            );
+
+            if do_button(
+                group,
+                ButtonSpec {
+                    id: Submit,
+                    rect: unscaled::Rect {
+                        x: starting_money_rect.x + starting_money_rect.w,
+                        y: unscaled::Y(100),
+                        w: unscaled::W(50),
+                        h: unscaled::H(100),
+                    },
+                    text: b"submit",
+                }
+            ) {
+                dbg!("submit");
+            } else {
+                let menu = [BackToTitleScreen, PlayerCountSelect, StartingMoneySelect, Submit];
+
+                match group.ctx.hot {
+                    BackToTitleScreen => {
+                        let menu_i = 0;
+
+                        match input.dir_pressed_this_frame() {
+                            Some(Dir::Up | Dir::Left) => {},
+                            Some(Dir::Down | Dir::Right) => {
+                                group.ctx.set_next_hot(menu[menu_i + 1]);
+                            }
+                            None => {}
+                        }
+                    }
+                    StartingMoneySelect => {
+                        let menu_i = 2;
+                        match input.dir_pressed_this_frame() {
+                            Some(Dir::Up) => {
+                                *starting_money = starting_money.saturating_add(MIN_MONEY_UNIT.get());
+                            },
+                            Some(Dir::Down) => {
+                                *starting_money = starting_money.saturating_sub(MIN_MONEY_UNIT.get());
+                                if *starting_money == 0 {
+                                    *starting_money = MIN_MONEY_UNIT.get();
+                                }
+                            },
+                            Some(Dir::Left) => {
+                                group.ctx.set_next_hot(menu[menu_i - 1]);
+                            }
+                            Some(Dir::Right) => {
+                                group.ctx.set_next_hot(menu[menu_i + 1]);
+                            }
+                            None => {}
+                        }
+                    }
+                    PlayerCountSelect => {
+                        let menu_i = 1;
+                        match input.dir_pressed_this_frame() {
+                            Some(Dir::Up) => {
+                                *player_count = player_count.saturating_add(1);
+                            },
+                            Some(Dir::Down) => {
+                                *player_count = player_count.saturating_sub(1);
+                            },
+                            Some(Dir::Left) => {
+                                group.ctx.set_next_hot(menu[menu_i - 1]);
+                            }
+                            Some(Dir::Right) => {
+                                group.ctx.set_next_hot(menu[menu_i + 1]);
+                            }
+                            None => {}
+                        }
+                    }
+                    Submit => {
+                        let menu_i = menu.len() - 1;
+                        match input.dir_pressed_this_frame() {
+                            Some(Dir::Left) => {
+                                group.ctx.set_next_hot(menu[menu_i - 1]);
+                            }
+                            Some(Dir::Right) => {}
+                            _ => {}
+                        }
+                    }
+                    Zero => {
+                        group.ctx.set_next_hot(PlayerCountSelect);
+                    }
+                    _ => {}
+                }
             }
         }
     }
